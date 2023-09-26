f6414cce7e62a26826dd309a0b63b3a5c70ebec4
ochandoca <ochandoca@esat-alumni.com>
2023-09-26 15:46:22 +0200

    Remove pause in script

--- tools/build_dependencies.bat
+++ tools/build_dependencies.bat
@@ -5,5 +5,3 @@ cd %~dp0..
 conan install -if build/deps/Release -s build_type=Release -s compiler="Visual Studio" -s compiler.runtime=MD --build=* ./
 conan install -if build/deps/RelWithDebInfo -s build_type=RelWithDebInfo -s compiler="Visual Studio" -s compiler.runtime=MD --build=* ./
 conan install -if build/deps/Debug -s build_type=Debug -s compiler="Visual Studio" -s compiler.runtime=MDd --build=* ./
-
-pause
