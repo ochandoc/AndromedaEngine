d76b8232e7018994734fda7916e27ef585fa05f9
j-maestre <xemaiestacio@gmail.com>
2023-09-26 12:36:16 +0200

    testing changes

--- examples/Sandbox/src/main.cpp
+++ examples/Sandbox/src/main.cpp
@@ -1,20 +1,25 @@
 #include "Core/Window.h"
+#include "Graphics/GraphicsContext.h"
 
 int main(int argc, char** argv)
 {
   WindowCreationInfo WindowInfo;
   WindowInfo.width = 1024;
   WindowInfo.height = 720;
+  WindowInfo.api = GraphicsAPI_OpenGL;
   WindowInfo.title = "Andromeda Engine";
 
 
   std::shared_ptr<Window> window = Window::Create(WindowInfo);
+  std::shared_ptr<GraphicsContext> g_context = window->create_context();
+  g_context->create_info();
 
-  while (window->is_open())
-  {
+  while (window->is_open()){
+
     window->update();
   }
   
+
   
   return 0;
 }
