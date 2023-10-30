9d1033d418a7a7b760521ed5dc5a6ecd2b63f042
ochandoca <ochandoca@esat-alumni.com>
2023-10-30 15:59:27 +0100

    Fix uint64 & int64 typedef

--- include/types.h
+++ include/types.h
@@ -3,9 +3,9 @@
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
-typedef unsigned long uint64;
+typedef unsigned long long uint64;
 
 typedef char int8;
 typedef short int16;
 typedef int int32;
-typedef long int64;
+typedef long long int64;
