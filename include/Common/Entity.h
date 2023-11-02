95c09f9030fae9ed925645e0ec8c94272fd68695
ochandoca <ochandoca@esat-alumni.com>
2023-11-02 18:34:22 +0100

    Add entity class

--- include/Common/Entity.h
+++ include/Common/Entity.h
@@ -1,8 +1,25 @@
 #pragma once
 
 #include "base.h"
+#include "Common/ID.h"
 
 namespace And
 {
-	typedef uint64 Entity;
+  class Entity
+  {
+  public:
+    Entity();
+    Entity(const Entity& other);
+    Entity(Entity&&);
+
+    ~Entity();
+
+    Entity& operator =(const Entity& other);
+    Entity& operator =(Entity&& other);
+
+    const ID& get_id() const { return m_Id; }
+
+  private:
+    ID m_Id;
+  };
 }
