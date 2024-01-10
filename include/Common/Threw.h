b90ab7a09d65f1e8ce8c023eda9b882935a6688e
xema <xemaieslestacio@gmail.com>
2024-01-10 19:48:41 +0100

    add extension on threw class

--- include/Common/Threw.h
+++ include/Common/Threw.h
@@ -19,6 +19,20 @@ namespace And{
           fclose(f);
         }
       }
+      
+      Threw(const char* file_name, const T& t, const char* extension){
+        FILE *f;
+        std::string name = std::string(file_name) + extension;
+        f = fopen(name.c_str(), "wb");
+
+        if(f == NULL){
+          printf("\n*** Error writing file ***\n");
+          //return false;
+        }else{
+          fwrite(&t, sizeof(T), 1, f);
+          fclose(f);
+        }
+      }
 
 
       Threw(const Threw&) = delete;
