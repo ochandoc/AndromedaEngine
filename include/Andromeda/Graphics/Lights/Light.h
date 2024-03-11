1ecd0711ef7c96a1719a4be5fee184abc82d4dc6
Xema <xemaiestacio@gmail.com>
2024-03-11 11:38:10 +0100

    add notes

--- include/Andromeda/Graphics/Lights/Light.h
+++ include/Andromeda/Graphics/Lights/Light.h
@@ -19,6 +19,8 @@ class Light : public ComponentBase{
         // Returns all light data in void*
         virtual void* GetData() = 0;
         virtual float* GetProjectViewMatrix(float aspect_ratio) = 0;
+        //virtual float* GetProjectMatrix(float aspect_ratio) = 0;
+        //virtual float* GetViewMatrix(float aspect_ratio) = 0;
 
         void SetCastShadows(bool enabled) { m_cast_shadows = enabled; }
         bool GetCastShadows(){return m_cast_shadows;}
@@ -29,7 +31,7 @@ class Light : public ComponentBase{
     protected:
 
         bool m_cast_shadows;
-        bool m_must_recalculate;
+        bool m_must_recalculate = true;
 
     private:
 
