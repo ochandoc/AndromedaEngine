da5690a290566fb18af3ba2f2d0daaf37ab779e7
j-maestre <xemaieslestacio@gmail.com>
2024-03-12 17:25:16 +0100

    add doxygen documentation

--- include/Andromeda/Graphics/Lights/Light.h
+++ include/Andromeda/Graphics/Lights/Light.h
@@ -20,14 +20,56 @@ class Light : public ComponentBase{
         Light(const Light&) = delete;
         Light(Light&&) = default;
 
-        // Returns all light data in void*
+        /**
+         * @brief Get all light data in raw format void*
+         * 
+         * @return void* 
+         */
         virtual void* GetData() = 0;
+
+        /**
+         * @brief Recalcule all matrix (its called automatically)
+         * 
+         * @param aspect_ratio 
+         */
         virtual void Recalculate(float aspect_ratio) = 0;
+
+        /**
+         * @brief Returns Projection * View Matrix
+         * 
+         * @param aspect_ratio 
+         * @return float* 
+         */
         virtual float* GetProjectViewMatrix(float aspect_ratio) = 0;
+
+        /**
+         * @brief Returns Projections Matrix
+         * 
+         * @param aspect_ratio 
+         * @return float* 
+         */
         virtual float* GetProjectMatrix(float aspect_ratio) = 0;
+
+        /**
+         * @brief Returns View Matrix
+         * 
+         * @param aspect_ratio 
+         * @return float* 
+         */
         virtual float* GetViewMatrix(float aspect_ratio) = 0;
 
+        /**
+         * @brief Set Cast Shadow bool
+         * 
+         */
         void SetCastShadows(bool enabled) { m_cast_shadows = enabled; }
+
+        /**
+         * @brief Get the Cast Shadows bool
+         * 
+         * @return true 
+         * @return false 
+         */
         bool GetCastShadows(){return m_cast_shadows;}
 
         friend class SpotLight;
